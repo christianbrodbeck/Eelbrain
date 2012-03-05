9d29a2db376ac8b03feef037ea65a8e44b9c9eb7
Christian Brodbeck <christian@Christian-Brodbecks-MacBook-Pro.local>
2012-03-05 01:53:43 -0500

    load diff created by kit2fiff

--- eelbrain/vessels/load.py
+++ eelbrain/vessels/load.py
@@ -1,5 +1,9 @@
 '''
-Functions for loading datasets from different types of files.
+Functions for loading datasets from different types of files. The most recent 
+workflow is based on loading events and data from a single file, and employs 
+:func:`fiff_events` and :func:`fiff_epochs` as in the following example::
+
+    >>> 
 
 
 Created on Feb 21, 2012
@@ -15,9 +19,10 @@ import mne
 import data as _data
 import colorspaces as _cs
 import sensors
+from eelbrain import ui
 
 
-__all__ = ['fiff', 'fiff_events', 'add_fiff_to_dataset']
+__all__ = ['fiff_events', 'fiff_epochs']
 
 
 
@@ -29,7 +34,107 @@ _default_fiff_properties = {'proj': 'ideal',
                             }
 
 
-def fiff_events(path, labels={}):
+def fiff_events(source_path=None, name=None):
+    """
+    Returns a dataset containing events from a raw fiff file. Use
+    :func:`fiff_epochs` to load MEG data corresponding to those events.
+    
+    source_path : str (path)
+        the location of the raw file (if ``None``, a file dialog will be 
+        displayed).
+    
+    name : str
+        A name for the dataset.
+    """
+    if source_path is None:
+        source_path = ui.ask_file("Pick a Fiff File", "Pick a Fiff File",
+                                  ext=[('fif', 'Fiff')])
+    
+    if name is None:
+        name = os.path.basename(source_path)
+    
+    raw = mne.fiff.Raw(source_path)
+    events = mne.find_events(raw)
+    if any(events[:,1] != 0):
+        raise NotImplementedError("Events starting with ID other than 0")
+        # this was the case in the raw-eve file, which contained all event 
+        # offsets, but not in the raw file created by kit2fiff. For handling
+        # see :func:`fiff_event_file`
+    istart = _data.var(events[:,0], name='i_start')
+    event = _data.var(events[:,2], name='eventID')
+    info = {'source': source_path}
+    return _data.dataset(event, istart, name=name, info=info)
+
+
+def fiff_epochs(dataset, i_start='i_start', 
+                tstart=-.2, tstop=.6, baseline=(None,  0),
+                properties=None, name="MEG", sensorsname='fiff-sensors'):
+    """
+    Uses the events in ``dataset[i_start]`` to extract epochs from the raw 
+    file
+    
+    i_start : str
+        name of the variable containing the index of the events to be
+        imported
+         
+    """
+    events = np.empty((dataset.N, 3), dtype=np.int32)
+    events[:,0] = dataset[i_start].x
+    events[:,1] = 0
+    events[:,2] = 1
+    
+    source_path = dataset.info['source']
+    raw = mne.fiff.Raw(source_path)
+    
+    # parse sensor net
+    sensor_list = []
+    for ch in raw.info['chs']:
+        ch_name = ch['ch_name']
+        if ch_name.startswith('MEG'):
+            x, y, z = ch['loc'][:3]
+            sensor_list.append([x, y, z, ch_name])
+    sensor_net = sensors.sensor_net(sensor_list, name=sensorsname)
+    
+    picks = mne.fiff.pick_types(raw.info, meg=True, eeg=False, stim=False, 
+                                eog=False, include=[], exclude=[])
+    
+    epochs = mne.Epochs(raw, events, 1, tstart, tstop, picks=picks, 
+                        baseline=baseline)
+    
+    # determine data container properties
+    epoch_shape = (len(picks), len(epochs.times))
+    data_shape = (len(events), len(epochs.times), len(picks))
+    # mne.fiff.raw.read_raw_segment uses float32
+    data = np.empty(data_shape, dtype='float32') 
+
+    # read the data
+    for i, epoch in enumerate(epochs):
+        if epoch.shape == epoch_shape:
+            data[i] = epoch.T
+        else:
+            msg = ("Epoch %i shape mismatch: does your epoch definition "
+                   "result in an epoch that overlaps the end of your data "
+                   "file?" % i)
+            raise IOError(msg)
+    
+    # read data properties
+    props = {'samplingrate': epochs.info['sfreq'][0]}
+    props.update(_default_fiff_properties)
+    if properties:
+        props.update(properties)
+    
+    T = epochs.times
+    timevar = _data.var(T, 'time')
+    dims = (timevar, sensor_net)
+    
+    dataset.add(_data.ndvar(dims, data, properties=props, name=name))
+    dataset.default_DV = name
+
+        
+
+
+
+def fiff_event_file(path, labels={}):
     events = mne.read_events(path).reshape((-1,6))
     name = os.path.basename(path)
     assert all(events[:,1] == events[:,5])
@@ -46,7 +151,11 @@ def fiff_events(path, labels={}):
 
 def add_fiff_to_events(path, dataset, i_start='i_start', 
                        tstart=-.2, tstop=.6, properties=None, 
-                       name="MEG", sensorsname='fiff-sensors'):#, i_stop='i_stop'):
+                       name="MEG", sensorsname='fiff-sensors'):
+    """
+    Adds MEG data form a new file to a dataset containing events. 
+    
+    """
     events = np.empty((dataset.N, 3), dtype=np.uint32)
     events[:,0] = dataset[i_start].x
     events[:,1] = 0
@@ -89,6 +198,9 @@ def fiff(raw, events, conditions, varname='condition', dataname='MEG',
          tstart=-.2, tstop=.6, properties=None, name=None, c_colors={},
          sensorsname='fiff-sensors'):
     """
+    Loads data directly when two files (raw and events) are provided 
+    separately.
+    
     conditions : dict
         ID->name dictionary of conditions that should be imported
     event : str
