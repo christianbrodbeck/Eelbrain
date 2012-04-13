94839004b645eb9d5c153b51e68ad272735316d1
Christian Brodbeck <christian@128-122-70-248.dynapool.nyu.edu>
2012-04-13 11:37:08 -0400

    ADD: old load.tsv() function

--- eelbrain/vessels/load.py
+++ eelbrain/vessels/load.py
@@ -13,7 +13,7 @@ import os
 
 import numpy as np
 
-__all__ = []
+__all__ = ['tsv']
 unavailable = []
 try:
     import mne
@@ -191,3 +191,102 @@ def fiff_epochs(dataset, i_start='i_start', target="MEG", add=True,
         dataset.default_DV = target
     else:
         return ndvar
+
+
+def tsv(path=None, names=True, types='auto', empty='nan', delimiter=None):
+    """
+    returns a ``dataset`` with data from a tab-separated values file. 
+    
+     
+    Arguments
+    ---------
+    
+    names :
+    
+    * ``True``: look for names on the first line if the file
+    * ``[name1, ...]`` use these names
+    * ``False``: use "v1", "v2", ...
+        
+    types :
+    
+    * ``'auto'`` -> import as var if all values can be converted float, 
+      otherwise as factor
+    * list of 0=auto, 1=factor, 2=var. e.g. ``[0,1,1,0]``
+    
+    empty :
+        value to substitute for empty cells
+    delimiter : str
+        value delimiting cells in the input file (None = any whitespace; 
+        e.g., ``'\\t'``)
+    
+    """
+    if path is None:
+        path = ui.ask_file("Select file to import as dataframe", 
+                           "Select file to import as dataframe")
+        if not path:
+            return
+    
+    with open(path) as f:
+        # read / create names
+        if names == True:
+            names = f.readline().split(delimiter)
+            names = [n.strip('"') for n in names]
+        
+        lines = []
+        for line in f:
+            values = []
+            for v in line.split(delimiter):
+                v = v.strip()
+                if not v:
+                    v = empty
+                values.append(v)
+            lines.append(values)
+    
+    n_vars = len(lines[0])
+    
+    if not names:
+        names = ['v%i'%i for i in xrange(n_vars)]
+    
+    n = len(names)
+    # decide whether to drop first column 
+    if n_vars == n:
+        start = 0
+    elif n_vars == n + 1:
+        start = 1
+    else:
+        raise ValueError("number of header different from number of data")
+    
+    if types in ['auto', None, False, True]:
+        types = [0]*n
+    else:
+        assert len(types) == n
+    
+    # prepare for reading data
+    data = []
+    for _ in xrange(n):
+        data.append([])
+    
+    # read rest of the data
+    for line in lines:
+        for i, v in enumerate(line[start:]):
+            for str_del in ["'", '"']:
+                if v[0] == str_del:
+                    v = v.strip(str_del)
+                    types[i] = 1
+            data[i].append(v)
+    
+    factors = []
+    
+    for name, values, force_type in zip(names, data, types):
+        v = np.array(values)
+        if force_type in [0,2]:
+            try:
+                v = v.astype(float)
+                f = _data.var(v, name=name)
+            except:
+                f = _data.factor(v, name=name)
+        else:
+            f = _data.factor(v, name=name)
+        factors.append(f)
+        
+    return _data.dataset(*factors)
