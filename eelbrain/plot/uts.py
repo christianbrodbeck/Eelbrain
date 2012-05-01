d3aca4565be73b2616eb124865ae1af10b1ec4f2
Christian Brodbeck <christian@128-122-70-247.dynapool.nyu.edu>
2012-04-30 20:54:55 -0400

    ENH: plot.uts.stat()
    (formerly plot.uts.uts())

--- eelbrain/plot/uts.py
+++ eelbrain/plot/uts.py
@@ -7,27 +7,31 @@ Created on Mar 13, 2012
 
 @author: christian
 '''
+from __future__ import division
+
 
 import numpy as np
 import scipy.stats
+import matplotlib.cm as _cm
 import matplotlib.pyplot as plt
 
 import eelbrain.vessels.data as _data
 
 
-__hide__ = ['plt']
+__hide__ = ['plt', 'division']
 
 
-def stat(var='Y', conditions=None, ct=np.mean, dev=scipy.stats.sem, ds=None,
-         figsize=(6,3), dpi=90):
+def stat(var='Y', X=None, main=np.mean, dev=scipy.stats.sem, ds=None,
+         figsize=(6,3), dpi=90, legend='upper right', title=True,
+         xdim='time', cm=_cm.jet):
     """
     Plots statistics for a one-dimensional ndvar
     
     var : ndvar
         dpendent variable (one-dimensiona ndvar)
-    conditions : categorial variable or None
+    X : categorial or None
         conditions which should be plotted separately
-    ct : func
+    main : func | 'all' | float
         central tendency (function that takes an ``axis`` argument)
     dev : str
         Measure for deviation: 
@@ -40,58 +44,80 @@ def stat(var='Y', conditions=None, ct=np.mean, dev=scipy.stats.sem, ds=None,
     """
     if isinstance(var, basestring):
         var = ds[var]
-    if isinstance(conditions, basestring):
-        conditions = ds[conditions]
+    if title is True:
+        title = var.name
+    if isinstance(X, basestring):
+        X = ds[X]
+        
     
     
     fig = plt.figure(figsize=figsize, dpi=dpi)
-    plt.subplots_adjust(.1, .2, .95, .95, .1, .4)
+    top = .9 # - bool(title) * .1
+    plt.subplots_adjust(.1, .1, .95, top, .1, .4)
     ax = plt.axes()
     
-    if conditions:
-        conditions = _data.asfactor(conditions)
-        labels = conditions.cells.values()
-        groups = [var[conditions==c] for c in labels]
+    if X:
+        X = _data.ascategorial(X)
+        values = X.values()
+        groups = [var[X==v] for v in values]
     else:
-        labels = [None]
+        values = [None]
         groups = [var]
     
     h = []
-    for group, lbl in zip(groups, labels):
-        _h = _plt_uts(ax, group, ct, dev, label=lbl)
+    legend_h = []
+    legend_lbl = []
+    N = len(groups)
+    for i, group, value in zip(range(N), groups, values):
+        lbl = str(value)
+        c = cm(i / N)
+        _h = _plt_stat(ax, group, main, dev, label=lbl, xdim=xdim, color=c)
         h.append(_h)
+        legend_h.append(_h['main'][0])
+        legend_lbl.append(lbl)
     
-    dim = var.dims[0]
+    dim = var.get_dim(xdim)
+    if title:
+        ax.set_title(title)
     ax.set_xlabel(dim.name)
     ax.set_xlim(min(dim), max(dim))
-    ax.legend()
+    if legend and any(values):
+        fig.legend(legend_h, legend_lbl, loc=legend)
     
     fig.show()
     return fig
 
 
 
-def _plt_uts(ax, ndvar, ct, dev, label=None, **kwargs):
+def _plt_stat(ax, ndvar, main, dev, label=None, xdim='time', color=None, **kwargs):
     h = {}
-    x = ndvar.dims[0].x
+    dim = ndvar.get_dim(xdim)
+    x = dim.x
+    y = ndvar.get_data(('epoch', 'time'))
     
-    ct_kwargs = kwargs.copy()
+    main_kwargs = kwargs.copy()
     if label:
-        ct_kwargs['label'] = label
+        main_kwargs['label'] = label
+    if color:
+        main_kwargs['color'] = color
+        kwargs['alpha'] = .3
+        kwargs['color'] = color
+    if isinstance(main, float):
+        main_kwargs['alpha'] = main
+        main = 'all'
     
-    if ct == 'all':
-        h['ct'] = ax.plot(x, ndvar.x.T, **ct_kwargs)
+    if main == 'all':
+        h['main'] = ax.plot(x, y.T, **main_kwargs)
         dev = None
-    elif hasattr(ct, '__call__'):
-        y = ct(ndvar.x, axis=0)
-        h['ct'] = ax.plot(x, y, **ct_kwargs)
+    elif hasattr(main, '__call__'):
+        y_ct = main(y, axis=0)
+        h['main'] = ax.plot(x, y_ct, **main_kwargs)
     else:
-        raise ValueError("Invalid argument: ct=%r" % ct)
+        raise ValueError("Invalid argument: main=%r" % main)
     
-    kwargs['alpha'] = .2
     if hasattr(dev, '__call__'):
-        ydev = dev(ndvar.x, axis=0)
-        h['dev'] = ax.fill_between(x, y-ydev, y+ydev, **kwargs)
+        ydev = dev(y, axis=0)
+        h['dev'] = ax.fill_between(x, y_ct-ydev, y_ct+ydev, **kwargs)
     else:
         pass
     
