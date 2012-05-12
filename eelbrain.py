629c41b78bf3121b7d6f5de9f1cc2185c82317d9
Christian Brodbeck <christian@Christian-Brodbecks-MacBook-Pro.local>
2012-05-12 13:52:02 -0400

    ENH app: re-enable logging of debug messages

--- eelbrain.py
+++ eelbrain.py
@@ -5,6 +5,11 @@ Startup script for wxterm.
 import matplotlib as mpl
 mpl.use('WXAgg')
 
+# configure the logging module so it logs debug messages
+# On OS X they can be viewed in the Console
+import logging
+logging.basicConfig(level=logging.DEBUG)
+
 import eelbrain.wxterm.app as _app_module
 
 
