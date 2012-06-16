47b30d87b689b957907e566b75a3f942edd5c511
Christian Brodbeck <christianmbrodbeck@gmail.com>
2012-06-16 19:11:17 -0400

    ADD: plot.sensors.multi() with ROI selection

--- eelbrain/plot/sensors.py
+++ eelbrain/plot/sensors.py
@@ -1,3 +1,9 @@
+'''
+Plotting functions for sensor_net instances.
+
+
+@author: christianmbrodbeck
+'''
 
 import numpy as np
 import matplotlib as mpl
@@ -36,7 +42,7 @@ def _ax_map2d_fast(ax, sensor_net, proj='default',
     return h
 
     
-def _ax_map2d(ax, sensor_net, proj='default', 
+def _ax_map2d(ax, sensor_net, proj='default', extent=1,
               frame = .02,
               kwargs=dict(
                           marker='x', # symbol
@@ -50,14 +56,14 @@ def _ax_map2d(ax, sensor_net, proj='default',
     ax.set_frame_on(False)
     ax.set_axis_off()
     
-    h = _plt_map2d(ax, sensor_net, proj=proj, kwargs=kwargs)
+    h = _plt_map2d(ax, sensor_net, proj=proj, extent=extent, kwargs=kwargs)
     
     ax.set_xlim(-frame, 1+frame)
     return h
     
 
 
-def _plt_map2d(ax, sensor_net, proj='default', ROI=None,
+def _plt_map2d(ax, sensor_net, proj='default', extent=1, ROI=None,
                kwargs=dict(
                            marker='.', # symbol
                            color='k', # mpl plot kwargs ...
@@ -66,7 +72,7 @@ def _plt_map2d(ax, sensor_net, proj='default', ROI=None,
                            ls='',
                            ),
                ):
-    locs = sensor_net.getLocs2d(proj=proj)
+    locs = sensor_net.getLocs2d(proj=proj, extent=extent)
     if ROI is not None:
         locs = locs[ROI]
     
@@ -114,6 +120,215 @@ def _plt_map2d_labels(ax, sensor_net, proj='default',
         handles.append(h)
     
     return handles
+
+
+
+class multi(mpl_canvas.CanvasFrame):
+    """
+    Select ROIs
+    ----------- 
+    
+     - Dragging with the left mouse button adds sensors to the ROI.
+     - Dragging with the right mouse button removes sensors from the current ROI.
+     - The 'Clear' button (or self.clear()) removes the ROI.
+    
+    
+    Methods
+    ------- 
+    
+    clear :
+        Clear the current ROI selection.
+    get_ROI : list
+        Returns the current ROI as a list of indices.
+    set_ROI
+        Set the current ROI with a list of indices.
+    
+    """
+    def __init__(self, sensors, size=7, dpi=100, frame=.05, ROI=[], proj='default'):
+        parent = wx.GetApp().shell
+        title = "Sensor Net: %s" % getattr(sensors, 'name', '')
+        super(multi, self).__init__(parent, title=title, 
+                                    figsize=(size, size), dpi=dpi)
+        
+        # in case sensor_net parent is submitted
+        if hasattr(sensors, 'sensors'):
+            sensors = sensors.sensors
+        elif hasattr(sensors, 'sensor'):
+            sensors = sensors.sensor
+        
+        # store args
+        self._sensors = sensors
+        
+        self.figure.set_facecolor('w')
+        self.figure.subplots_adjust(left=0, bottom=0, right=1, top=1,
+                                    wspace=.1, hspace=.1)
+        
+        ext = np.vstack((sensors.locs.min(0), sensors.locs.max(0)))
+        aframe = np.array([-frame, frame])
+        xlim = ext[:,0] + aframe
+        ylim = ext[:,1] + aframe
+        zlim = ext[:,2] + aframe
+        
+        # back
+        ax = self.ax0 = self.figure.add_subplot(2,2,1)
+        ax.y_fmt = 'z = %.3g'
+        ax.x_fmt = 'x = %.3g'
+        ax.proj = 'y-'
+        ax.extent = False
+        self._h0 = _ax_map2d(ax, sensors, proj=ax.proj, extent=ax.extent)
+        
+        # left
+        ax = self.ax1 = self.figure.add_subplot(2,2,2, sharey=self.ax0)
+        ax.x_fmt = 'y = %.3g'
+        ax.y_fmt = 'z = %.3g'
+        ax.proj = 'x-'
+        ax.extent = False
+        self._h1 = _ax_map2d(ax, sensors, proj=ax.proj, extent=ax.extent)
+        
+        # top
+        ax = self.ax2 = self.figure.add_subplot(2,2,3, sharex=self.ax0)
+        ax.x_fmt = 'x = %.3g'
+        ax.y_fmt = 'y = %.3g'
+        ax.proj = 'z+'
+        ax.extent = False
+        self._h2 = _ax_map2d(ax, sensors, proj=ax.proj, extent=ax.extent)
+        
+        self.ax0.set_xlim(*xlim)
+        self.ax0.set_ylim(*zlim)
+        self.ax1.set_xlim(*zlim)
+        self.ax2.set_ylim(*ylim)
+        
+        # proj
+        ax = self.ax3 = self.figure.add_subplot(2,2,4)
+        ax.x_fmt = ' '
+        ax.y_fmt = ' '
+        ax.proj = proj
+        ax.extent = 1
+        self._h3 = _ax_map2d(ax, sensors, proj=ax.proj, extent=ax.extent)
+        self.ax3.set_xlim(-frame, 1+frame)
+        self.ax3.set_ylim(-frame, 1+frame)
+        
+        self.axes = [self.ax0, self.ax1, self.ax2, self.ax3]
+        self.Show()
+        
+        # ROI
+        self.ROI_kwargs = dict(marker='o', # symbol
+                               color='r', # mpl plot kwargs ...
+                               ms=5, # marker size
+                               markeredgewidth=.9,
+                               ls='')
+        self._ROI_h = []
+        if ROI:
+            self.set_ROI(ROI)
+        else:
+            self.ROI = None
+        
+        # setup mpl event handling
+        self.canvas.mpl_connect("button_press_event", self.on_button_press)
+        self.canvas.mpl_connect("button_release_event", self.on_button_release)
+        self.canvas.mpl_connect("motion_notify_event", self.on_motion)
+        self._drag_ax = None
+        self._drag_x = None
+        self._drag_y = None
+    
+    def _init_FillToolBar(self, tb):
+        tb.AddSeparator()
+        
+        # plot labels
+        btn = wx.Button(tb, wx.ID_CLEAR, "Clear")
+        tb.AddControl(btn)
+        self.Bind(wx.EVT_BUTTON, self.OnClear, btn)
+        
+        super(multi, self)._init_FillToolBar(tb)
+    
+    def clear(self):
+        "clear the ROI"
+        self.ROI = None
+        self.update_ROI_plot()
+    
+    def get_ROI(self):
+        if self.ROI is None:
+            return []
+        else:
+            return np.where(self.ROI)[0]
+    
+    def on_button_press(self, event):
+        ax = event.inaxes
+        if ax:
+            self._is_dragging = True
+            self._drag_ax = event.inaxes
+            self._drag_x = event.xdata
+            self._drag_y = event.ydata
+            
+            self.canvas.store_canvas()
+            x = np.ones(5) * event.xdata
+            y = np.ones(5) * event.ydata
+            self._drag_rect = ax.plot(x, y, '-k')[0]
+    
+    def on_button_release(self, event):
+        x = self._drag_rect.get_xdata()
+        y = self._drag_rect.get_ydata()
+        xmin = min(x) 
+        xmax = max(x)
+        ymin = min(y)
+        ymax = max(y)
+        
+        ax = self._drag_ax
+        locs = self._sensors.getLocs2d(ax.proj, extent=ax.extent)
+        x = locs[:,0]
+        y = locs[:,1]
+        sel = (x > xmin)  &  (x < xmax)  &  (y > ymin)  &  (y < ymax)
+        
+        if self.ROI is None:
+            self.ROI = sel
+        elif event.button == 1:
+            self.ROI[sel] = True
+        else:
+            self.ROI[sel] = False
+        
+        # clear dragging-related attributes
+        self._drag_rect.remove()
+        del self._drag_rect
+        self._drag_ax = None
+        self._drag_x = None
+        self._drag_y = None
+        
+        self.update_ROI_plot()
+    
+    def on_motion(self, event):
+        ax = event.inaxes
+        if ax and ax is self._drag_ax:
+            x0 = self._drag_x
+            x1 = event.xdata
+            y0 = self._drag_y
+            y1 = event.ydata
+            x = [x0, x1, x1, x0, x0]
+            y = [y0, y0, y1, y1, y0]
+            self._drag_rect.set_data(x, y)
+            self.canvas.redraw(artists=[self._drag_rect])
+    
+    def OnClear(self, event):
+        self.clear()
+    
+    def set_ROI(self, ROI):
+        self.ROI = np.zeros(len(self._sensors), dtype=bool)
+        self.ROI[ROI] = True
+        self.update_ROI_plot()
+    
+    def update_ROI_plot(self):
+        # remove old plots
+        while self._ROI_h:
+            self._ROI_h.pop(0).remove()
+        
+        # plot
+        if self.ROI is not None:
+            for ax in self.axes:
+                h = _plt_map2d(ax, self._sensors, proj=ax.proj, extent=ax.extent, 
+                               ROI=self.ROI, kwargs=self.ROI_kwargs)
+                self._ROI_h.extend(h)
+        
+        self.canvas.draw()
+
 
 
 
