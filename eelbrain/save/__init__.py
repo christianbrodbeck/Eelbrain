ad049b591a8b941abecefd7898c8267e22f8c353
Christian Brodbeck <christianmbrodbeck@gmail.com>
2012-06-17 21:17:53 -0400

    NEW: save.pickle

--- eelbrain/save/__init__.py
+++ eelbrain/save/__init__.py
@@ -1,1 +1,24 @@
+import os
+import cPickle
+
+from eelbrain import ui
 from _besa import *
+
+
+__hide__ = ['ui', 'cPickle', 'os']
+
+
+
+def pickle(obj, dest=None):
+    if dest is None:
+        dest = ui.ask_saveas("Pickle Destination", '', 
+                             ext=[('pickled', "Pickled Python Object")])
+        if dest:
+            print 'dest=%r' % dest
+        else:
+            return
+    else:
+        dest = os.path.expanduser(dest)
+    
+    with open(dest, 'w') as FILE:
+        cPickle.dump(obj, FILE)
