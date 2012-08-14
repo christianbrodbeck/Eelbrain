742d62272290ae128512f652da07f49cca85c587
Christian Brodbeck <christianmbrodbeck@gmail.com>
2012-08-14 18:16:08 -0400

    ADD: load.brainvision.events()

--- eelbrain/load/brainvision.py
+++ eelbrain/load/brainvision.py
@@ -10,7 +10,52 @@ Created on Jul 27, 2012
 import os
 import re
 
+import numpy as np
+
 from eelbrain import ui
+from eelbrain.vessels.data import dataset, factor, var
+
+
+__all__ = ['events', 'vhdr']
+
+
+section_re = re.compile('^\[([ \w]+)\]')
+#             <Type>,<Description>,<Position>,<Points>,<Channel Number>,<Date>
+marker_re = re.compile('^Mk(\d+)=([ \w]+),(\w)(\d+),(\d+),(\d+),(\d+)', re.MULTILINE)
+
+vhdr_hdr = 'Brain Vision Data Exchange Header File'
+
+
+def events(vhdr_path=None):
+    if vhdr_path is None:
+        vhdr_path = ui.ask_file("Pick a Brain Vision EEG Header File", 
+                                "Pick a Brain Vision EEG Header File",
+                                ext=[('vhdr', 'Brain Vision Header File')])
+        if not vhdr_path:
+            return
+    
+    hdr = vhdr(vhdr_path)
+    if hdr.markerfile is None:
+        raise IOError("No marker file referenced in %r" % vhdr_path)
+    elif hdr.DataType == 'FREQUENCYDOMAIN':
+        raise NotImplementedError
+    
+    txt = open(hdr.markerfile).read()
+    m = marker_re.findall(txt)
+    m = np.array(m)
+    
+    name, _ = os.path.split(os.path.basename(hdr.path))
+    ds = dataset(name=name)
+    ds['Mk'] = var(np.array(m[:,0], dtype=int))
+    ds['event_type'] = factor(m[:,1])
+    ds['event_ID'] = var(np.array(m[:,3], dtype=int))
+    ds['i_start'] = var(np.array(m[:,4], dtype=int))
+    ds['points'] = var(np.array(m[:,5], dtype=int))
+    ds['channel'] = var(np.array(m[:,6], dtype=int))
+    
+    ds.info['hdr'] = hdr
+    ds.info['samplingrate'] = hdr.samplingrate
+    return ds
 
 
 class vhdr(dict):
@@ -29,26 +74,31 @@ class vhdr(dict):
             if not path:
                 raise RuntimeError("User Canceled")
 
-        section_re = re.compile('^\[([ \w]+)\]')
         kv_re = re.compile('^(\w+)=(.+)', re.MULTILINE)
         
         hdr_section = {}
-        for line in open(path):
-            m = section_re.match(line)
-            if m:
-                name = m.group(1)
-                hdr_section = self[name] = {}
-            else:
-                m = kv_re.match(line)
+        with open(path) as FILE:
+            if not FILE.readline().startswith(vhdr_hdr):
+                err = ("Not a brain vision vhdr file: %r (needs to start with"
+                       " %r" % (path, vhdr_hdr))
+                raise IOError(err)
+            for line in FILE:
+                m = section_re.match(line)
                 if m:
-                    key = m.group(1)
-                    value = m.group(2).strip()
-                    hdr_section[key] = value
+                    name = m.group(1)
+                    hdr_section = self[name] = {}
+                else:
+                    m = kv_re.match(line)
+                    if m:
+                        key = m.group(1)
+                        value = m.group(2).strip()
+                        hdr_section[key] = value
         
         self.path = path
         info = self['Common Infos']
-        self.dirname, self.basename = os.path.split(path)
         
+        # paths
+        self.dirname, self.basename = os.path.split(path)
         datafile = info['DataFile']
         if os.path.isabs(datafile):
             self.datafile = datafile
@@ -63,6 +113,13 @@ class vhdr(dict):
                 self.markerfile = os.path.join(self.dirname, markerfile)
         else:
             self.markerfile = None
+        
+        # other info
+        self.DataFormat = info.get('DataFormat', 'ASCII')
+        self.DataOrientation = info.get('DataOrientation', 'MULTIPLEXED')
+        self.DataType = info.get('DataType', 'TIMEDOMAIN')
+        self.NumberOfChannels = int(info['NumberOfChannels'])
+        self.samplingrate = 1e6 / float(info['SamplingInterval'])
     
     def __repr__(self):
         return "vhdr(%r)" % self.path
