2d0546b7d9914bd5886b6f33116b4d85b9411081
christianmbrodbeck <christianmbrodbeck@gmail.com>
2012-09-09 01:37:53 -0400

    FIX example

--- examples/ndvar/topo.py
+++ examples/ndvar/topo.py
@@ -1,7 +1,7 @@
 import numpy as np
 from eelbrain.eellab import *
 import eelbrain.vessels.colorspaces as cs
-import eelbrain.vessels.sensors
+from eelbrain.vessels.sensors import sensor_net
 
 
 """
@@ -18,8 +18,8 @@ x = np.random.normal(0,1,(30,5,len(T)))
 x[15:,:3,20:40] += np.hanning(20) * 2
 
 # create the sensor dimension from 5 sensor locations in 3d space
-sensor = V.sensors.sensor_net([[0,0,0],[1,0,0],[0,-1,0],[-1,0,0],[0,1,0]], 
-                              name='testnet', transform_2d=None)
+sensor = sensor_net([[0,0,0],[1,0,0],[0,-1,0],[-1,0,0],[0,1,0]], 
+                    name='testnet', transform_2d=None)
 
 # combine all these into the ndvar
 Y = ndvar(x, dims=('case', sensor, T), name='Y', 
@@ -33,7 +33,7 @@ subject = factor(xrange(15), tile=2, random=True, name='subject')
 
 
 if 01: # plot topographically an uncorrected t-test
-    plot.topo.array(spm.ttest(Y, A, match=subject))
+    plot.topo.array(testnd.ttest(Y, A, match=subject))
 
 if 01: # and a butterfly plot
-    plot.topo.butterfly(spm.ttest(Y, A, match=subject))
+    plot.topo.butterfly(testnd.ttest(Y, A, match=subject))
