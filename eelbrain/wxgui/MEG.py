206f410f60094ae6d9ab4d01e800bca57e4568c8
Christian Brodbeck <christianmbrodbeck@gmail.com>
2012-10-01 18:30:30 -0400

    cleanup: autoformat

--- eelbrain/wxgui/MEG.py
+++ eelbrain/wxgui/MEG.py
@@ -40,15 +40,15 @@ class select_cases_butterfly(eelfigure):
                  mean=True, topo=True, ylim=None, fill=True, aa=False, dpi=50):
         """
         Plots all cases in the collection segment and allows visual selection
-        of cases. The selection can be retrieved through the get_selection 
-        Method.  
-        
+        of cases. The selection can be retrieved through the get_selection
+        Method.
+
         Arguments
         ----------
-        
+
         dataset : dataset
             dataset on which to perform the selection.
-        
+
         topo : bool
             Show a dynamically updated topographic plot at the bottom right
 
@@ -63,19 +63,19 @@ class select_cases_butterfly(eelfigure):
             antialiasing (matplolibt parameter)
 
         Example::
-        
+
             >>> select_cases_butterfly(my_dataset)
             [... visual selection of cases ...]
             >>> cases = my_dataset['reject'] == False
             >>> pruned_dataset = my_dataset[cases]
-        
+
         """
     # interpret plotting args
         # variable keeping track of selection
         if isinstance(data, basestring):
             data = dataset[data]
         self._data = data
-        
+
         if isinstance(target, basestring):
             try:
                 target = dataset[target]
@@ -84,20 +84,20 @@ class select_cases_butterfly(eelfigure):
                 target = _data.var(x, name=target)
                 dataset.add(target)
         self._target = target
-        
+
     # prepare segments
         self._nplots = nplots
         n_per_page = self._n_per_page = np.prod(nplots) - 2
         n_pages = dataset.N // n_per_page + bool(dataset.N % n_per_page)
         self._n_pages = n_pages
-        
+
         # get a list of IDS for each page
         self._segs_by_page = []
         for i in xrange(n_pages):
             start = i * n_per_page
             stop = min((i + 1) * n_per_page, dataset.N)
             self._segs_by_page.append(range(start, stop))
-        
+
     # init wx frame
         title = "select_cases_butterfly -> %r" % target.name
         figsize = (plotsize[0] * nplots[0], plotsize[1] * nplots[1])
@@ -117,7 +117,7 @@ class select_cases_butterfly(eelfigure):
             ylim = data.properties.get('ylim', None)
         if ylim:
             self._bfly_kwargs['ylim'] = ylim
-        
+
     # finalize
         self._dataset = dataset
         self.show_page(0)
@@ -126,7 +126,7 @@ class select_cases_butterfly(eelfigure):
 
     def _fill_toolbar(self, tb):
         tb.AddSeparator()
-        
+
         # --> select page
         txt = wx.StaticText(tb, -1, "Page:")
         tb.AddControl(txt)
@@ -149,7 +149,7 @@ class select_cases_butterfly(eelfigure):
         tb.AddLabelTool(wx.ID_REFRESH, "Refresh", Icon("tango/actions/view-refresh"))
         tb.Bind(wx.EVT_TOOL, self._Refresh, id=wx.ID_REFRESH)
         tb.AddSeparator()
-        
+
         # Thresholding
         btn = wx.Button(tb, ID.THRESHOLD, "Threshold")
         tb.AddControl(btn)
@@ -161,8 +161,8 @@ class select_cases_butterfly(eelfigure):
         index[seg_IDs] = True
         index *= self._target
         mseg = self._data[index].mean()
-        return mseg 
-    
+        return mseg
+
     def _update_mean(self):
         mseg = self._get_page_mean_seg()
         data = mseg.get_data(('time', 'sensor'))
@@ -172,14 +172,14 @@ class select_cases_butterfly(eelfigure):
         Y = np.empty((Ylen, 2))
         # data
         Y[:T_len, 1] = data.min(1)
-        Y[2*T_len:T_len:-1, 1] = data.max(1)
+        Y[2 * T_len:T_len:-1, 1] = data.max(1)
         # T
         Y[:T_len, 0] = T
-        Y[2*T_len:T_len:-1, 0] = T
+        Y[2 * T_len:T_len:-1, 0] = T
         # border regions
-        Y[T_len,:] = Y[T_len+1,:]
+        Y[T_len, :] = Y[T_len + 1, :]
         self._mean_handle[0].set_paths([Y])
-        
+
         # update figure
         self._frame.redraw(axes=[self._mean_ax])
 
@@ -194,7 +194,7 @@ class select_cases_butterfly(eelfigure):
 
         self._frame.redraw(artists=[h])
         self._update_mean()
-    
+
     def invert_selection(self, axID):
         "ID refers to ax-ID in the display"
         # find current selection
@@ -202,26 +202,26 @@ class select_cases_butterfly(eelfigure):
         epochID = ax.segID
         state = not self._target[epochID]
         self._target[epochID] = state
-        
+
         # update plot
         self.set_ax_state(axID, state)
-    
+
     def set_ylim(self, y):
         for ax in self._case_axes:
             ax.set_ylim(-y, y)
         self._mean_ax.set_ylim(-y, y)
         self.canvas.draw()
         self._bfly_kwargs['ylim'] = y
-    
+
     def show_page(self, page):
         "Dislay a specific page (start counting with 0)"
         self._current_page_i = page
         self._page_choice.Select(page)
-        
+
         self.figure.clf()
         nx, ny = self._nplots
         seg_IDs = self._segs_by_page[page]
-        
+
         # segment plots
         self._case_handles = []
         self._case_axes = []
@@ -233,7 +233,7 @@ class select_cases_butterfly(eelfigure):
                 color = 'k'
             else:
                 color = 'r'
-            ax = self.figure.add_subplot(nx, ny, i+1, xticks=[0], yticks=[])#, 'axis_off')
+            ax = self.figure.add_subplot(nx, ny, i + 1, xticks=[0], yticks=[])#, 'axis_off')
             ax._epoch_state = state
 #            ax.set_axis_off()
             h = plot.utsnd._ax_butterfly(ax, [case], color=color, antialiased=False,
@@ -244,20 +244,20 @@ class select_cases_butterfly(eelfigure):
             self._case_handles.append(h)
             self._case_axes.append(ax)
             self._case_segs.append(case)
-        
-        
+
+
         # mean plot
-        ax = self._mean_ax = self.figure.add_subplot(nx, ny, nx*ny)
+        ax = self._mean_ax = self.figure.add_subplot(nx, ny, nx * ny)
         ax.ID = -1
-        
+
         mseg = self._mean_seg = self._get_page_mean_seg()
         self._mean_handle = plot.utsnd._ax_butterfly(ax, [mseg], color='k', **self._bfly_kwargs)
-        
+
         # topomap
-        ax = self._topo_ax = self.figure.add_subplot(nx, ny, nx*ny - 1)
+        ax = self._topo_ax = self.figure.add_subplot(nx, ny, nx * ny - 1)
         ax.ID = -2
         ax.set_axis_off()
-        
+
         self.canvas.draw()
 
     def _OnBackward(self, event):
@@ -284,7 +284,7 @@ class select_cases_butterfly(eelfigure):
     def _on_leave_axes(self, event):
         sb = self.GetStatusBar()
         sb.SetStatusText("", 0)
-    
+
     def _get_statusbar_text(self, event):
         "called by parent class to get figure-specific status bar text"
         ax = event.inaxes
@@ -347,16 +347,16 @@ class select_cases_butterfly(eelfigure):
     def _OnThreshold(self, event):
         """
         above: True, False, None
-            how to mark segments that exceed the threshold: True->good; 
+            how to mark segments that exceed the threshold: True->good;
             False->bad; None->don't change
         below:
             same as ``above`` but for segments that do not exceed the threshold
-        
+
         """
         threshold = None
         above = False
         below = True
-        
+
         msg = "What value should be used to threshold the data?"
         while threshold is None:
             dlg = wx.TextEntryDialog(self, msg, "Choose Threshold", "2e-12")
@@ -369,9 +369,9 @@ class select_cases_butterfly(eelfigure):
                                "a float." % value, '!')
             else:
                 return
-        
-        process.mark_by_threshold(self._dataset, DV=self._data, 
-                                  threshold=threshold, above=above, 
+
+        process.mark_by_threshold(self._dataset, DV=self._data,
+                                  threshold=threshold, above=above,
                                   below=below, target=self._target)
 
         self._Refresh(event)
@@ -379,26 +379,26 @@ class select_cases_butterfly(eelfigure):
 
 
 class pca(mpl_canvas.CanvasFrame):
-    def __init__(self, dataset, Y='MEG', nplots=(7,10), dpi=50, figsize=(20,12)):
+    def __init__(self, dataset, Y='MEG', nplots=(7, 10), dpi=50, figsize=(20, 12)):
         """
         Performs PCA and opens a GUI for removing individual components.
-        
+
         Y : ndvar | str
             dependent variable
         timecourse : int
-            number of (randomly picked) segments for whichthe time-course is 
+            number of (randomly picked) segments for whichthe time-course is
             displayed
-         
+
         """
         if isinstance(Y, basestring):
             Y = dataset[Y]
-        
+
         self._dataset = dataset
         self._Y = Y
-        
+
     # prepare plots:
         self._topo_kwargs = {}
-        
+
     # wx stuff
         parent = wx.GetApp().shell
         title = "PCA of %r" % Y.name
@@ -407,12 +407,12 @@ class pca(mpl_canvas.CanvasFrame):
         # connect
         self.canvas.mpl_connect('button_press_event', self.OnClick)
         # figure
-        self.figure.subplots_adjust(left=.01, right=.99, bottom=.05, top=.95, 
+        self.figure.subplots_adjust(left=.01, right=.99, bottom=.05, top=.95,
                                     hspace=.2)
-        
+
     # do the PCA
         pca = self.pca = process.PCA(Y)
-        
+
     # plot the components
         self._components = []
         self._rm_comp = []
@@ -421,28 +421,28 @@ class pca(mpl_canvas.CanvasFrame):
         for i in xrange(np.prod(nplots)):
             name = title_temp % i
             comp = pca.get_component(i)
-            ax = self.figure.add_subplot(npy, npx, i+1, xticks=[], yticks=[])
+            ax = self.figure.add_subplot(npy, npx, i + 1, xticks=[], yticks=[])
             ax.Id = i
             plot.topo._ax_topomap(ax, [comp])
             ax.set_title(name)
             ax.set_frame_on(1)
             ax.set_axis_on()
             self._components.append(comp)
-        
+
     # finalize
         self.canvas.store_canvas()
         self.Show()
-    
+
     def _init_FillToolBar(self, tb):
         tb.AddSeparator()
-        
+
         # remove
         btn = wx.Button(tb, ID.PCA_REMOVE, "Remove Selected Components")
         tb.AddControl(btn)
         self.Bind(wx.EVT_BUTTON, self.OnRemove, id=ID.PCA_REMOVE)
-                        
+
         mpl_canvas.CanvasFrame._init_FillToolBar(self, tb)
-    
+
     def OnClick(self, event):
         ax = event.inaxes
         if ax:
@@ -454,20 +454,20 @@ class pca(mpl_canvas.CanvasFrame):
                 self._rm_comp.append(Id)
                 ax.set_axis_bgcolor('r')
             self.canvas.redraw(axes=[ax])
-    
+
     def OnRemove(self, event):
         target = None
         rm = sorted(self._rm_comp)
         while not target:
             dlg = wx.TextEntryDialog(self, "What name should the new ndvar be assigned in the dataset?",
-                                     "Choose Name for New Variable", "%s"%self._Y.name)
+                                     "Choose Name for New Variable", "%s" % self._Y.name)
             if dlg.ShowModal() == wx.ID_OK:
                 newname = str(dlg.GetValue())
                 if newname in self._dataset:
                     msg = ("The dataset already contains an item named %r. "
                            "Should it be replaced? The item is:\n\n%r" %
                             (newname, self._dataset[newname]))
-                    answer = ui.ask("Replace %r?"%newname, msg)
+                    answer = ui.ask("Replace %r?" % newname, msg)
                     if answer is True:
                         target = newname
                     elif answer is None:
@@ -478,12 +478,12 @@ class pca(mpl_canvas.CanvasFrame):
                     target = newname
             else:
                 return
-        
+
         # if we made it down here, remove the component:
         self._dataset[target] = self.pca.subtract(rm, name=target)
-        
+
         self.Close()
 
 
 
-        
+
