41a9d48253500b2b973c387246563d88d0a29ac6
Christian Brodbeck <christianmbrodbeck@gmail.com>
2013-02-01 15:45:26 -0500

    ENH dev_mri coreg plot: show dig-fiducials even if no mri fiducials found

--- eelbrain/plot/coreg.py
+++ eelbrain/plot/coreg.py
@@ -488,10 +488,11 @@ class dev_mri(object):
                 self.mri_fid = geom_fid(dig, unit='m')
                 self.mri_fid.set_T(mri_dev_t)
                 self.mri_fid.plot_points(fig, scale=0.005)
-                self.dig_fid = geom_fid(raw.info['dig'], unit='m')
-                self.dig_fid.set_T(head_dev_t)
-                self.dig_fid.plot_points(fig, scale=0.04, opacity=.25,
-                                         color=(.5, .5, 1))
+
+            self.dig_fid = geom_fid(raw.info['dig'], unit='m')
+            self.dig_fid.set_T(head_dev_t)
+            self.dig_fid.plot_points(fig, scale=0.04, opacity=.25,
+                                     color=(.5, .5, 1))
 
         self.view()
 
