8a466c15e8329cbb91010b2a176ace071bbdc192
Christian Brodbeck <christianmbrodbeck@gmail.com>
2013-02-14 17:16:43 -0500

    STY: code formatter

--- eelbrain/vessels/process.py
+++ eelbrain/vessels/process.py
@@ -18,21 +18,21 @@ def rm_pca(ds, rm=[], source='MEG', target='MEG', baseline=None):
     """
     Perform PCA and remove certain components. Use gui.pca to find components
     initially.
-    
+
     baseline : None | tuple(tstart, tend)
         Baseline correction after subtracting the PCA components. The baseline
         is specified as a (tstart, tend) tuple. None includes all values until the
         start/end of the epoch (see :func:`.rm_baseline`).
-    
+
     """
     if not rm:
         raise ValueError("No components selected")
     if isinstance(source, basestring):
         source = ds[source]
-    
+
     rm = sorted(rm)
     n_comp = max(rm) + 1
-    
+
     node = PCA(source, n_comp=n_comp)
     ds[target] = node.subtract(rm, name=target, baseline=baseline)
 
@@ -46,71 +46,71 @@ class PCA:
             source data
         n_comp : None | int
             number of components to compute (None = all)
-        
+
         """
         self.source = source
         data = self._source_data = source.get_data(('case', 'time', 'sensor'))
         self._source_dtype = data.dtype
         data = data.astype(_np.float64)
-        
+
         # do the pca
         node = self.node = _mdp.nodes.PCANode(output_dim=n_comp)
         for epoch in data:
             node.train(epoch)
         node.stop_training()
-        
+
         # project into the pca space
         n_epochs, n_t, n_sensors = data.shape
         old_data = data.reshape((n_epochs * n_t, n_sensors))
         self._proj = self.node.execute(old_data)
-        
-        
+
+
     def get_component(self, i):
         dims = self.source.get_dims(('sensor',))
-        data = self.node.v.T[i,]
+        data = self.node.v.T[i, ]
         name = 'comp_%i' % i
         ndvar = _data.ndvar(data, dims, name=name)
         return ndvar
-    
+
     def get_load(self, i):
         time = self.source.get_dim('time')
-        dims = ('case', time, )
+        dims = ('case', time,)
         shape = (len(self.source), len(time))
-        data = self._proj[:,i].reshape(shape)
+        data = self._proj[:, i].reshape(shape)
         name = 'comp_%i_load' % i
         ndvar = _data.ndvar(data, dims=dims, name=name)
         return ndvar
-    
+
     def subtract(self, components, baseline=(None, 0), name='{name}'):
         """
-        returns a copy of the source ndvar with the principal 
-        components specified in ``components`` removed. 
-        
+        returns a copy of the source ndvar with the principal
+        components specified in ``components`` removed.
+
         Arguments:
-        
+
         components : list of ints
             list of components to remove
         baseline : True | False | (int|None, int|None)
             Baseline correction after subtracting the components. True -> use the
-            settings stored in the ndvar.properties; False -> do not apply any 
-            baseline correction; a new baseline can be specified with a tuple of 
+            settings stored in the ndvar.properties; False -> do not apply any
+            baseline correction; a new baseline can be specified with a tuple of
             two time values or None (use all values until the end of the epoch).
-        
+
         """
         data = self._source_data
         proj = self._proj
-        
+
         # flatten retained components
         for i in xrange(proj.shape[1]):
             if i not in components:
-                proj[:,i] = 0 
-        
+                proj[:, i] = 0
+
         # remove the components
         rm_comp_data = self.node.inverse(proj)
         new_data = data - rm_comp_data.reshape(data.shape)
         new_data = new_data.astype(self._source_dtype)
-        
-        # create the output new ndvar 
+
+        # create the output new ndvar
         dims = ('case',) + self.source.get_dims(('time', 'sensor'))
         properties = self.source.properties
         name = name.format(name=self.source.name)
@@ -122,33 +122,33 @@ class PCA:
 
 
 
-def mark_by_threshold(dataset, DV='MEG', threshold=2e-12, above=False, below=None, 
+def mark_by_threshold(dataset, DV='MEG', threshold=2e-12, above=False, below=None,
                       target='accept'):
     """
-    Marks epochs based on a threshold criterion (any sensor exceeding the 
-    threshold at any time) 
-    
+    Marks epochs based on a threshold criterion (any sensor exceeding the
+    threshold at any time)
+
     above: True, False, None
-        How to mark segments that exceed the threshold: True->good; 
+        How to mark segments that exceed the threshold: True->good;
         False->bad; None->don't change
-    
+
     below:
         Same as ``above`` but for segments that do not exceed the threshold
-    
+
     threshold : float
-        The threshold value. 
+        The threshold value.
         1.25e-11: detect saturated channels
-        2e-12: conservative final rejection 
-    
+        2e-12: conservative final rejection
+
     target : factor or str
-        Factor (or its name) in which the result is stored. If ``var`` is 
-        a string and the dataset does not contain that factor, it is 
+        Factor (or its name) in which the result is stored. If ``var`` is
+        a string and the dataset does not contain that factor, it is
         created.
-    
+
     """
     if isinstance(DV, basestring):
         DV = dataset[DV]
-    
+
     # get the factor on which to store results
     if _data.isfactor(target) or _data.isvar(target):
         assert len(target) == dataset.N
@@ -161,13 +161,13 @@ def mark_by_threshold(dataset, DV='MEG', threshold=2e-12, above=False, below=Non
             dataset.add(target)
     else:
         raise ValueError("target needs to be a factor")
-    
+
     # do the thresholding
     if _data.isndvar(DV):
         for ID in xrange(dataset.N):
             data = DV[ID]
             v = _np.max(_np.abs(data.x))
-            
+
             if v > threshold:
                 if above is not None:
                     target[ID] = above
@@ -176,7 +176,7 @@ def mark_by_threshold(dataset, DV='MEG', threshold=2e-12, above=False, below=Non
     else:
         for ID in xrange(dataset.N):
             v = DV[ID]
-            
+
             if v > threshold:
                 if above is not None:
                     target[ID] = above
@@ -188,7 +188,7 @@ def mark_by_threshold(dataset, DV='MEG', threshold=2e-12, above=False, below=Non
 def rm_baseline(ndvar, tstart=None, tend=0, name='{name}'):
     """
     returns an ndvar object with baseline correction applied.
-    
+
     ndvar : ndvar
         the source data
     tstart : scalar | None
@@ -197,15 +197,15 @@ def rm_baseline(ndvar, tstart=None, tend=0, name='{name}'):
         the end of the baseline  period (None -> the end of the epoch)
     name : str
         name for the new ndvar
-    
+
     """
     subdata = ndvar.subdata(time=(tstart, tend))
     baseline = subdata.summary('time')
-    
+
     t_ax = subdata.get_axis('time')
     index = (slice(None),) * t_ax + (None,)
     bl_data = baseline.x[index]
-    
+
     dims = ndvar.dims
     data = ndvar.x - bl_data
     name = name.format(name=ndvar.name)
