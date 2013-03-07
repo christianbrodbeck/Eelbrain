christianbrodbeck/Eelbrain

commit 32d8103c39fc897c17602fec34acca73453f1fb2

FIX mne_experiment.set(): only match values for variables in var_values

eelbrain/vessels/experiment.py
@@ -1663,7 +1663,8 @@ class mne_experiment(object):
         # test var_value
         if match:
             for k, v in kwargs.iteritems():
-                if (v not in self.var_values.get(k, [])) and not ('*' in v):
+                if ((k in self.var_values) and (not '*' in v)
+                     and v not in self.var_values.get[k]):
                     raise ValueError("Variable %r has not value %r" % (k, v))
 
         # set state

commit 5eda43d710fa3ee617e71df88ed83ebd77516780

FIX mne_experiment.set(): bug introduced in 32d8103

eelbrain/vessels/experiment.py
@@ -1709,7 +1709,7 @@ class mne_experiment(object):
         if match:
             for k, v in kwargs.iteritems():
                 if ((k in self.var_values) and (not '*' in v)
-                     and v not in self.var_values.get[k]):
+                     and v not in self.var_values[k]):
                     raise ValueError("Variable %r has not value %r" % (k, v))
 
         # set state