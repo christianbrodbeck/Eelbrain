3fc603e7e732dd8d04f61eca06a286ce10ee256a
Christian Brodbeck <christianmbrodbeck@gmail.com>
2013-06-02 15:35:03 -0400

    FIX  plot.opo.topomap():  typo in default vmax arg

--- eelbrain/plot/topo.py
+++ eelbrain/plot/topo.py
@@ -55,7 +55,7 @@ class topomap(_base.eelfigure):
                            interpolation=interpolation,
                            proj=proj,
                            sensors=sensors,
-                           vmax=max,
+                           vmax=vmax,
                            )
 
         self._subplots = []
