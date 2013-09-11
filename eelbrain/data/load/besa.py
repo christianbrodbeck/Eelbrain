ba2c75d08de567ee4575a397da73d24e7ef33ce3
Christian Brodbeck <christianmbrodbeck@gmail.com>
2013-09-11 18:12:55 -0400

    ENH load:  add load.besa.mrat_data()

--- eelbrain/data/load/besa.py
+++ eelbrain/data/load/besa.py
@@ -65,3 +65,76 @@ def roi_results(path=None, varname=None):
     ds['subject'] = Factor(map(str, xrange(n_s)), tile=n_c, random=True)
 
     return ds
+
+
+def mrat_data(path=None, tstart=-0.1, roi=None, varname=None):
+    """
+    Load meg data from a saved mrat dataset object
+
+    Parameters
+    ----------
+    path : str | None
+        Path to the *.m file containing the saved results. If None, a file
+        can be selected using a system file dialog.
+    tstart : scalar
+        Time value of the first sample in the data.
+    roi : numpy index
+        Index of the sources to load (Python style indexing, i.e., the first
+        source has index 0).
+    varname : str | None
+        If the .m file contains more than one variable, the name of the
+        variable containing the results can be specified as string.
+
+    Returns
+    -------
+    mrat_data : Dataset
+        A Dataset containing the following variables: 'meg' the meg ROI
+        waveforms; 'condition' the condition label; 'subject' a subject
+        identifier.
+    """
+    if path is None:
+        path = ui.ask_file("Pick a Matlab file with Besa-MN results",
+                           "Pick a Matlab File",
+                           ext=[('mat', 'Matlab data file')])
+        if not path:
+            return
+
+    m = loadmat(path)
+    if varname is None:
+        keys = [k for k in m.keys() if not k.startswith('_')]
+        if len(keys) == 0:
+            raise ValueError("Noe data in .mat file")
+        elif len(keys) == 1:
+            varname = keys[0]
+        else:
+            err = ("More than one variables in .mat file. Use the varname "
+                   "parameter.")
+            raise ValueError(err)
+    data = m[varname]
+    conds = [c[0] for c in data['conditionNames'][0][0][0]]
+
+    xs = []
+    n_s = None
+    for cond in conds:
+        if n_s is None:
+            n_s = len(data['data'][0][0][cond][0][0][0])
+        x_ = np.array([data['data'][0][0][cond][0][0][0][i]
+                       for i in xrange(n_s)])
+        xs.append(x_)
+    x = np.concatenate(xs)
+
+    if roi is None:
+        raise NotImplementedError("Need to specify roi parameter")
+    else:
+        x = np.mean(x[:, roi], 1)
+
+    ds = Dataset()
+    _, n_samples = x.shape
+    time = UTS(tstart, .001, n_samples)
+    ds['meg'] = NDVar(x, dims=('case', time))
+
+    ds['condition'] = Factor(conds, rep=n_s)
+
+    ds['subject'] = Factor(map(str, xrange(n_s)), tile=len(conds), random=True)
+
+    return ds
