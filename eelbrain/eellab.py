a6680614bbe6cf0f245761d38c744319a2cc4fb7
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-02-12 14:30:16 -0500

    STY:  eellab imports * from lab

--- eelbrain/eellab.py
+++ eelbrain/eellab.py
@@ -1,18 +1,4 @@
 '''
-The eelbrain.eellab module contains a collection of Eelbrain components that
-are useful for interactive data analysis.
+Duplicate eelbrain.lab for backwards compatibility
 '''
-from .data import *
-
-# mne
-import eelbrain.data.load.fiff
-
-# mayavi
-try:
-    import eelbrain.data.plot.brain
-    import eelbrain.data.plot.coreg
-except:
-    globals().setdefault('err', []).append('plot.brain (mayavi)')
-
-from . import ui
-from . import gui
+from .lab import *
