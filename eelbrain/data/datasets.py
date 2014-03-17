ab58f2a4484437705c74859c2bfa5631dc444c35
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-03-16 21:29:26 -0400

    ENH datasets:  add MNE loader

--- eelbrain/data/datasets.py
+++ eelbrain/data/datasets.py
@@ -1,10 +1,109 @@
 '''
 Defines some basic example datasets that are used in testing.
 '''
+import os
+
 import numpy as np
 
+import mne
+from mne import minimum_norm as mn
+
+from . import load
 from .data_obj import Dataset, Factor, Var, NDVar, Sensor, UTS
 from .design import permute
+
+
+def get_mne_sample(tmin=-0.1, tmax=0.4, baseline=(None, 0), sns=False,
+                   inv=False, src=False, sub="modality=='A'", fixed=False,
+                   snr=2, method='dSPM'):
+    """Load events and epochs from the MNE sample data
+
+    Parameters
+    ----------
+    tmin, tmax baseline :
+        Epoch parameters.
+    sns : bool
+        Add sensor space data as NDVar as ``ds['sns']``.
+    inv : bool
+        Add the inverse operator as ``ds.info['inv']`` even when not adding
+        source estimates.
+    src : bool
+        Add source space data as NDVar as ``ds['src']``.
+    sub : str | None
+        Expresion for subset of events to load.
+    fixed : bool
+        MNE inverse parameter.
+    snr : scalar
+        MNE inverse parameter.
+    method : str
+        MNE inverse parameter.
+
+    Returns
+    -------
+    ds : Dataset
+        Dataset with epochs from the MNE sample dataset.
+    """
+    data_dir = mne.datasets.sample.data_path()
+    meg_dir = os.path.join(data_dir, 'MEG', 'sample')
+    raw_file = os.path.join(meg_dir, 'sample_audvis_filt-0-40_raw.fif')
+    subjects_dir = os.path.join(data_dir, 'subjects')
+    subject = 'sample'
+    label_path = os.path.join(subjects_dir, subject, 'label', '%s.label')
+
+    ds = load.fiff.events(raw_file, stim_channel='STI 014')
+    ds.info['subjects_dir'] = subjects_dir
+    ds.info['subject'] = subject
+    ds.info['label'] = label_path
+
+    # get the trigger variable form the dataset for eaier access
+    trigger = ds['trigger']
+
+    # use trigger to add various labels to the dataset
+    ds['condition'] = Factor(trigger, labels={1:'LA', 2:'RA', 3:'LV', 4:'RV',
+                                              5:'smiley', 32:'button'})
+    ds['side'] = Factor(trigger, labels={1: 'L', 2:'R', 3:'L', 4:'R',
+                                         5:'None', 32:'None'})
+    ds['modality'] = Factor(trigger, labels={1: 'A', 2:'A', 3:'V', 4:'V',
+                                             5:'None', 32:'None'})
+
+    if sub:
+        ds = ds.sub(sub)
+
+    load.fiff.add_mne_epochs(ds, tmin, tmax, baseline)
+    if sns:
+        ds['sns'] = load.fiff.epochs_ndvar(ds['epochs'])
+
+    if inv or src:
+        bem_dir = os.path.join(subjects_dir, subject, 'bem')
+        trans_file = os.path.join(meg_dir, 'sample_audvis_raw-trans.fif')
+        bem_file = os.path.join(bem_dir, 'sample-5120-5120-5120-bem-sol.fif')
+        src_file = os.path.join(bem_dir, 'sample-ico-4-src.fif')
+        fwd_file = os.path.join(meg_dir, 'sample-ico-4-fwd.fif')
+
+        epochs = ds['epochs']
+        if os.path.exists(fwd_file):
+            fwd = mne.read_forward_solution(fwd_file)
+        else:
+            if os.path.exists(src_file):
+                src_ = src_file
+            else:
+                src_ = mne.setup_source_space(subject, src_file, 'ico4',
+                                              subjects_dir=subjects_dir)
+
+            fwd = mne.make_forward_solution(epochs.info, trans_file, src_,
+                                            bem_file, fwd_file)
+
+        cov_file = os.path.join(meg_dir, 'sample_audvis-cov.fif')
+        cov = mne.read_cov(cov_file)
+        inv = mn.make_inverse_operator(epochs.info, fwd, cov, None, None,
+                                       fixed)
+        ds.info['inv'] = inv
+
+    if src:
+        stcs = mn.apply_inverse_epochs(epochs, inv, 1. / (snr ** 2), method)
+        ds['src'] = load.fiff.stc_ndvar(stcs, subject, 'ico-4', subjects_dir)
+
+    return ds
 
 
 def get_rand(utsnd=False):
