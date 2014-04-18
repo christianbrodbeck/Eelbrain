b4e79331c3e72fae51d764ba968b69d44b14d3e0
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-04-18 12:21:16 -0400

    EXA:  lower number of samples in permutation test

--- examples/ndvar/uts cluster permutation test.py
+++ examples/ndvar/uts cluster permutation test.py
@@ -10,9 +10,9 @@ T = UTS(-.2, .01, 100)
 y = np.random.normal(0, .5, (60, len(T)))
 
 # add an interaction effect
-y[:15,20:60] += np.hanning(40) * 1
+y[:15, 20:60] += np.hanning(40) * 1
 # add a main effect
-y[:30,50:80] += np.hanning(30) * 1
+y[:30, 50:80] += np.hanning(30) * 1
 
 
 Y = NDVar(y, dims=('case', T), name='Y')
@@ -21,17 +21,18 @@ B = Factor(['b0', 'b1'], rep=15, tile=2, name='B')
 
 
 # fixed effects model
-res = testnd.anova(Y, A*B, samples=1000)
+# (increase the number of samples for a more accurate result)
+res = testnd.anova(Y, A * B, samples=100)
 plot.UTSClusters(res, title="Fixed Effects Model")
 
 
 # random effects model:
 subject = Factor(range(15), tile=4, random=True, name='subject')
-res = testnd.anova(Y, A*B*subject, samples=1000, match=subject)
+res = testnd.anova(Y, A * B * subject, samples=100, match=subject)
 plot.UTSClusters(res, title="Random Effects Model")
 
 # plot Y
-p = plot.UTSStat(Y, A%B, match=subject, clusters=res.clusters)
+p = plot.UTSStat(Y, A % B, match=subject, clusters=res.clusters)
 
 # table with all clusters
 print res.clusters
