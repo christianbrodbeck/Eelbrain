fa1ea149986c7ae8176552506e2fec2c5d45c98b
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-07-15 00:26:19 -0400

    API:  remove irrelevant items from eelbrain.testnd

--- eelbrain/testnd.py
+++ eelbrain/testnd.py
@@ -54,4 +54,5 @@ cluster enhancement [2]_.
 
 __test__ = False
 
-from ._stats.testnd import *
+from ._stats.testnd import (t_contrast_rel, corr, ttest_1samp, ttest_ind,
+                            ttest_rel, anova)
