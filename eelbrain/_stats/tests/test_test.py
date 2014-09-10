27b96252c7bc9e82883f581101408758823654ac
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-09-09 22:34:23 -0400

    FIX stats.test.star()

--- eelbrain/_stats/tests/test_test.py
+++ eelbrain/_stats/tests/test_test.py
@@ -1,6 +1,14 @@
 # Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+from numpy.testing import assert_array_equal
 
 from eelbrain import datasets, test
+from eelbrain._stats import test as _test
+
+
+def test_star():
+    "Test the star function"
+    assert_array_equal(_test.star([0.1, 0.04, 0.01], int, corr=None), [0, 1, 2])
+    assert_array_equal(_test.star([0.1, 0.04, 0.01], int), [0, 0, 1])
 
 
 def test_ttest():
