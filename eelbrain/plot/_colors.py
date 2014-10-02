954a362a12566e66e52199b941ef9fcf445989a5
Christian Brodbeck <christianmbrodbeck@gmail.com>
2014-10-02 16:28:49 -0400

    ENH plot-colors:  ColorGrid plot

--- eelbrain/plot/_colors.py
+++ eelbrain/plot/_colors.py
@@ -72,6 +72,74 @@ def colors_for_twoway(x1_cells, x2_cells, cmap=None):
     return colors
 
 
+class ColorGrid(_EelFigure):
+
+    def __init__(self, row_cells, col_cells, colors, row_first=None,
+                 **layout):
+        """Plot colors in a grid
+
+        Parameters
+        ----------
+        row_cells : tuple of str
+            Cells contained in the rows.
+        col_cells : tuple of str
+            Cells contained in the columns.
+        colors : dict
+            Colors for cells.
+        row_first : bool
+            Whether the row cell precedes the column cell in color keys. By
+            default this is inferred from the existing keys.
+        """
+        if row_first is None:
+            row_cell_0 = row_cells[0]
+            col_cell_0 = col_cells[0]
+            if (row_cell_0, col_cell_0) in colors:
+                row_first = True
+            elif (col_cell_0, row_cell_0) in colors:
+                row_first = False
+            else:
+                msg = ("Neither %s nor %s exist as a key in colors" %
+                       ((row_cell_0, col_cell_0), (col_cell_0, row_cell_0)))
+                raise KeyError(msg)
+
+        _EelFigure.__init__(self, "ColorGrid", None, 3, 1, layout)
+        ax = self.figure.add_axes((0, 0, 1, 1), frameon=False)
+        ax.set_axis_off()
+
+        # reverse rows so we can plot upwards
+        row_cells = tuple(reversed(row_cells))
+        n_rows = len(row_cells)
+        n_cols = len(col_cells)
+
+        # color patches
+        for col in xrange(n_cols):
+            for row in xrange(n_rows):
+                if row_first:
+                    cell = (row_cells[row], col_cells[col])
+                else:
+                    cell = (col_cells[col], row_cells[row])
+                patch = mpl.patches.Rectangle((col, row), 1, 1, fc=colors[cell],
+                                              ec='none')
+                ax.add_patch(patch)
+
+        # labels
+        y = n_rows + 0.1
+        for col in xrange(n_cols):
+            label = col_cells[col]
+            ax.text(col + 0.5, y, label, va='bottom', ha='left',
+                    rotation=40)
+        x = n_cols + 0.1
+        for row in xrange(n_rows):
+            label = row_cells[row]
+            ax.text(x, row + 0.5, label, va='center', ha='left')
+
+        ymax = n_rows + 4
+        ax.set_ylim(0, ymax)
+        ax.set_xlim(0, ymax * self._layout.w / self._layout.h)
+
+        self._show()
+
+
 class Colors(_EelFigure):
 
     def __init__(self, colors, cells=None, **layout):
