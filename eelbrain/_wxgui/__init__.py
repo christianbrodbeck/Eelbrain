7ea9fcbfd6da78b47a2784b0d25d69b8aed88942
Christian Brodbeck <christianmbrodbeck@gmail.com>
2015-09-01 15:39:14 -0400

    silence wx-python import warning

--- eelbrain/_wxgui/__init__.py
+++ eelbrain/_wxgui/__init__.py
@@ -1,2 +1,6 @@
+# silence ImportWarning
+import warnings
+warnings.filterwarnings('ignore', message='Not importing directory .*', module='wx.*')
+
 from .app import get_app, run
 from . import select_epochs
