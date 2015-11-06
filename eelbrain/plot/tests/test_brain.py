7c5bd65ef9bfc638da517ffebb213a95a2120315
Christian Brodbeck <christianmbrodbeck@gmail.com>
2015-11-06 14:33:50 -0500

    TEST plot.brain:  close figures

--- eelbrain/plot/tests/test_brain.py
+++ eelbrain/plot/tests/test_brain.py
@@ -9,14 +9,18 @@ def test_plot_brain():
     p = plot.brain.dspm(src)
     cb = p.plot_colorbar(show=False)
     cb.close()
+    p.close()
+    # not closing figures leads to weird interactions with the QT backend
 
     p = plot.brain.dspm(src, hemi='lh')
     cb = p.plot_colorbar(show=False)
     cb.close()
+    p.close()
 
     p = plot.brain.cluster(src, hemi='rh', views='parietal')
     cb = p.plot_colorbar(show=False)
     cb.close()
+    p.close()
 
     image = plot.brain.bin_table(src, tstart=0.1, tstop=0.3, tstep=0.1)
     print repr(image)
