1663cb671e0f9894c8e9b978989d8c78a38bae35
Christian Brodbeck <christianmbrodbeck@gmail.com>
2017-02-12 18:41:32 -0500

    API:  plot.brain.brain() to plot brain with or without data
    replaces and deprecates surfer_brain()

--- eelbrain/plot/brain.py
+++ eelbrain/plot/brain.py
@@ -1,5 +1,5 @@
 """Plot source space NDVars with mayavi/pysurfer."""
 # Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
 
-from ._brain import (cluster, dspm, surfer_brain, annot, bin_table,
+from ._brain import (brain, cluster, dspm, surfer_brain, annot, bin_table,
                      dspm_bin_table, copy, p_map, annot_legend)
