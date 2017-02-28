63249942bd3006364ac6bddfdcc835b30455df53
Christian Brodbeck <christianmbrodbeck@gmail.com>
2017-02-28 15:43:28 -0500

    CLEANUP:   remove unused definitions

--- eelbrain/_utils/__init__.py
+++ eelbrain/_utils/__init__.py
@@ -1,4 +1,4 @@
 # Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
-from .basic import (deprecated, IdDict, intervals, LazyProperty, keydefaultdict,
+from .basic import (deprecated, intervals, LazyProperty, keydefaultdict,
                     n_decimals, natsorted, log_level, set_log_level)
 from .system import caffeine
