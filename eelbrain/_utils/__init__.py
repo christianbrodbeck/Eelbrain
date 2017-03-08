161c95e3fd3b628221784afea73ec49c16243ab0
Christian Brodbeck <christianmbrodbeck@gmail.com>
2017-03-08 15:53:39 -0500

    LOG:  WrappedFormatter for screen-logger

--- eelbrain/_utils/__init__.py
+++ eelbrain/_utils/__init__.py
@@ -1,4 +1,5 @@
 # Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
-from .basic import (deprecated, intervals, LazyProperty, keydefaultdict,
-                    n_decimals, natsorted, log_level, set_log_level)
+from .basic import (
+    WrappedFormater, deprecated, intervals, LazyProperty,
+    keydefaultdict, n_decimals, natsorted, log_level, set_log_level)
 from .system import caffeine
