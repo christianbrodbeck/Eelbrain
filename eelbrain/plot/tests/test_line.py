b7745c408891bd6ce47dc6fadc9bb48382d8b1fb
Christian Brodbeck <christianmbrodbeck@gmail.com>
2017-05-02 17:07:59 -0400

    TEST plot.LineStack:  don't show; close

--- eelbrain/plot/tests/test_line.py
+++ eelbrain/plot/tests/test_line.py
@@ -6,8 +6,9 @@ from eelbrain import datasets, plot
 def test_linestack():
     "Test plot.LineStack()"
     ds = datasets.get_uts()
-    p = plot.LineStack(ds[:10, 'uts'])
+    p = plot.LineStack(ds[:10, 'uts'], show=False)
     ax = p.figure.axes[0]
     eq_(ax.get_xlim(), (-0.2, .79))
     p.set_xlim(.1, .4)
     eq_(ax.get_xlim(), (.1, .4))
+    p.close()
