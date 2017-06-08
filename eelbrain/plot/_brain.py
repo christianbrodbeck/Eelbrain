e5f9628f6dc06393b06bcd74e286edbcb61959b2
Christian Brodbeck <christianmbrodbeck@gmail.com>
2017-06-08 10:11:01 -0400

    FIX plot.brain:  ImageTable

--- eelbrain/plot/_brain.py
+++ eelbrain/plot/_brain.py
@@ -840,7 +840,7 @@ class ImageTable(EelFigure, ColorBarMixin):
 
     def __init__(self, n_rows, n_columns, title=None, *args, **kwargs):
         layout = ImLayout(n_rows * n_columns, 0, 0.5, 4/3, 2, title, *args,
-                          nrow=n_rows, ncol=n_columns, **kwargs)
+                          nrow=n_rows, ncol=n_columns, autoscale=True, **kwargs)
         EelFigure.__init__(self, None, layout)
 
         self._n_rows = n_rows
