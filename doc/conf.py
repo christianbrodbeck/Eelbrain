f5a10d78edd30f1cef4ecf627b0f1879f82f944c
Christian Brodbeck <christianmbrodbeck@gmail.com>
2017-06-25 10:23:27 -0400

    DOC code:  _templates -> templates

--- doc/conf.py
+++ doc/conf.py
@@ -42,7 +42,7 @@ autosummary_generate = True
 numpydoc_show_class_members = False
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = ['templates']
 
 # The suffix of source filenames.
 source_suffix = '.rst'
