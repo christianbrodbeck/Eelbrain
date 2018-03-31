3a4726edfb69d775bdc7e32552b9413d908b5eaa
Christian Brodbeck <christianmbrodbeck@gmail.com>
2018-03-30 22:31:02 -0400

    FIX plot.ColorBar:  width and left or top in margins

--- eelbrain/plot/_base.py
+++ eelbrain/plot/_base.py
@@ -1628,6 +1628,7 @@ class Layout(BaseLayout):
                 raise ValueError("w < axw")
 
         self.w_fixed = w or axw
+        self._margins_arg = margins
 
         if margins is True:
             use_margins = True
