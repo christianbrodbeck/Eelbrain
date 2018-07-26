a4506a992215668673f845925a250aa1de28d227
AD\proloy <proloy@umd.edu>
2018-07-26 13:05:25 -0400

    VolSourceSpace

--- eelbrain/_data_obj.py
+++ eelbrain/_data_obj.py
@@ -9377,6 +9377,10 @@ class VolumeSourceSpace(SourceSpaceBase):
             return SourceSpaceBase._dim_index(self, index)
 
 
+class VolumeSourceSpace(SourceSpace):
+    pass
+
+
 class UTS(Dimension):
     """Dimension object for representing uniform time series
 
