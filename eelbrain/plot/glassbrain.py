c0ed24d4b787dee88afc10cbb2efe4d13599be77
AD\proloy <proloy@umd.edu>
2018-08-28 22:56:27 -0400

    set gui backend in osx

--- eelbrain/plot/glassbrain.py
+++ eelbrain/plot/glassbrain.py
@@ -1,3 +1,17 @@
 # Author: Proloy Das <proloy@umd.edu>
 
+# osx
+from sys import platform
+if platform == "darwin":
+    import matplotlib
+    gui_env = ['WX', 'TkAgg', 'Qt4Agg', 'Qt5Agg', 'WXAgg']
+    for gui in gui_env:
+        try:
+            print("testing %s" %gui)
+            matplotlib.use(gui, warn=False, force=True)
+            break
+        except:
+            continue
+    print("Using: %s" %matplotlib.get_backend())
+
 from ._glassbrain import (GlassBrain, butterfly)
