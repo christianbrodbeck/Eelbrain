3fd1ccd4e96a83250f97f927bcca7865a68e328b
Christian Brodbeck <christianmbrodbeck@gmail.com>
2018-08-29 08:15:48 -0400

    lazy import of pyploy?

--- eelbrain/plot/glassbrain.py
+++ eelbrain/plot/glassbrain.py
@@ -1,17 +1,2 @@
 # Author: Proloy Das <proloy@umd.edu>
-
-# fix matplotlib backend for osx
-from sys import platform
-if platform == "darwin":
-    import matplotlib
-    gui_env = ['WX', 'TkAgg', 'Qt4Agg', 'Qt5Agg', 'WXAgg']
-    for gui in gui_env:
-        try:
-            print("testing %s" %gui)
-            matplotlib.use(gui, warn=False, force=True)
-            break
-        except:
-            continue
-    print("Using: %s" %matplotlib.get_backend())
-
 from ._glassbrain import (GlassBrain, butterfly)
