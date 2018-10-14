a3a61193824d43d76382d740e9a44840a6c598ba
Christian Brodbeck <christianmbrodbeck@gmail.com>
2018-10-14 10:27:01 -0400

    test_topo:  capture warnings

--- eelbrain/plot/tests/test_topo.py
+++ eelbrain/plot/tests/test_topo.py
@@ -1,6 +1,6 @@
 # Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
-from nose.tools import eq_, assert_raises
 import numpy as np
+import pytest
 
 from eelbrain import datasets, plot, testnd
 from eelbrain._utils.testing import requires_mne_sample_data
@@ -33,7 +33,8 @@ def test_plot_topomap_mne():
     p.close()
     # grad
     ds = datasets.get_mne_sample(sub=[0], sns='grad')
-    assert_raises(NotImplementedError, plot.Topomap, 'meg.sub(time=.1)', ds=ds, show=False)
+    with pytest.raises(NotImplementedError), pytest.warns(RuntimeWarning):
+        plot.Topomap('meg.sub(time=.1)', ds=ds, show=False)
 
 
 def test_plot_topo_butterfly():
@@ -50,9 +51,9 @@ def test_plot_topo_butterfly():
     p.close()
     p = plot.TopoButterfly('utsnd', mark=['1', '2'], ds=ds, show=False)
     p.set_vlim(2)
-    eq_(p.get_vlim(), (-2.0, 2.0))
+    assert p.get_vlim() == (-2.0, 2.0)
     p.set_ylim(-1, 1)
-    eq_(p.get_ylim(), (-1.0, 1.0))
+    assert p.get_ylim() == (-1.0, 1.0)
     p.close()
 
 
@@ -68,8 +69,7 @@ def test_plot_array():
     p.close()
 
     # results
-    res = testnd.ttest_ind('utsnd', 'A', ds=ds, pmin=0.05, tstart=0.1,
-                           tstop=0.3, samples=2)
+    res = testnd.ttest_ind('utsnd', 'A', ds=ds, pmin=0.05, tstart=0.1, tstop=0.3, samples=2)
     p = plot.TopoArray(res, show=False)
     p.set_topo_t(0, 0.)
     p.close()
