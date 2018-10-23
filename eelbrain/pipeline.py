345a7eb14ba06e5614c3149bfd5fe8d8fcac0abc
Christian Brodbeck <christianmbrodbeck@gmail.com>
2018-10-22 22:09:14 -0400

    ADD:  eelbrain.pipeline for epochs

--- eelbrain/pipeline.py
+++ eelbrain/pipeline.py
@@ -1,2 +1,3 @@
 from ._experiment.mne_experiment import MneExperiment
 from ._experiment.preprocessing import RawSource, RawFilter, RawICA, RawMaxwell, RawReReference
+from ._experiment.epochs import PrimaryEpoch, SecondaryEpoch, SuperEpoch
