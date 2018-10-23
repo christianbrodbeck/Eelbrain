d80a4a54d53909a1ccd2ab3c24cba4103b275b12
Christian Brodbeck <christianmbrodbeck@gmail.com>
2018-10-23 13:20:44 -0400

    ADD: eelbrain.pipeline for tests

--- eelbrain/pipeline.py
+++ eelbrain/pipeline.py
@@ -1,3 +1,4 @@
 from ._experiment.mne_experiment import MneExperiment
 from ._experiment.preprocessing import RawSource, RawFilter, RawICA, RawMaxwell, RawReReference
 from ._experiment.epochs import PrimaryEpoch, SecondaryEpoch, SuperEpoch
+from ._experiment.test_def import ANOVA, TTestOneSample, TTestInd, TTestRel, TContrastRel, TwoStageTest
