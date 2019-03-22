ac23e810dda206bcf43c6ae19b714dba7755a5f3
proloyd <proloy@umd.edu>
2019-03-22 16:58:09 -0400

    LIC remove the license statement from vector.pyx

--- eelbrain/_stats/vector.pyx
+++ eelbrain/_stats/vector.pyx
@@ -5,27 +5,8 @@
 """
 optimized statistics functions
 
-Contains code from 'Efficient numerical diagonalization of hermitian 3x3 matrices'
-governed by following license ( GNU LESSER GENERAL PUBLIC LICENSE Version 2.1)
-
-
-Copyright (C) 2008 Joachim Kopp
-All rights reserved.
-
-This library is free software; you can redistribute it and/or
-modify it under the terms of the GNU Lesser General Public
-License as published by the Free Software Foundation; either
-version 2.1 of the License, or (at your option) any later version.
-
-This library is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-Lesser General Public License for more details.
-
-You should have received a copy of the GNU Lesser General Public
-License along with this library; if not, write to the Free Software
-Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
-USA
+Relies on code from 'Efficient numerical diagonalization of hermitian 3x3 matrices'
+governed by GNU LESSER GENERAL PUBLIC LICENSE Version 2.1.
 """
 
 cimport cython
