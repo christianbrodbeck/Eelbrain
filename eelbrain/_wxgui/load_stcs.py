18d34de200a4b294b720e2a9ac516ac10e1d48d6
Samir Reddigari <samirreddigari@gmail.com>
2019-04-30 20:14:46 -0400

    STYLE load_stcs GUI

--- eelbrain/_wxgui/load_stcs.py
+++ eelbrain/_wxgui/load_stcs.py
@@ -106,8 +106,9 @@ class STCLoaderFrame(EelbrainFrame):
 
 
 class FactorPanel(wx.Panel):
+    """Panel to display a factor and its level names"""
     def __init__(self, parent, levels, idx, editable=True):
-        super().__init__(parent)
+        super(FactorPanel, self).__init__(parent)
         if editable:
             self.factor_ctl = wx.TextCtrl(self, value="factor_%d" % idx)
         else:
@@ -124,8 +125,9 @@ class FactorPanel(wx.Panel):
 
 
 class MRIPanel(wx.Panel):
+    """Panel containing MRI input fields (dir, subject, src)"""
     def __init__(self, parent):
-        super().__init__(parent)
+        super(MRIPanel, self).__init__(parent)
         sizer = wx.StaticBoxSizer(wx.VERTICAL, self, "")
         # check for subjects dir in environment
         sdir = os.environ.get("SUBJECTS_DIR")
