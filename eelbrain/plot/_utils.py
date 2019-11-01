69b1db3a1c644b67c00fc52a0e4965d6938f29f3
Christian Brodbeck <christianmbrodbeck@gmail.com>
2019-11-01 17:14:03 -0400

    ADD:  set_hsv() color utility

--- eelbrain/plot/_utils.py
+++ eelbrain/plot/_utils.py
@@ -9,3 +9,14 @@ def adjust_hsv(color, h=0., s=0., v=0.):
     hsv.hsv_s += s
     hsv.hsv_v += v
     return convert_color(hsv, sRGBColor).get_value_tuple()
+
+
+def set_hsv(color, h=None, s=None, v=None):
+    hsv = convert_color(sRGBColor(*to_rgb(color)), HSVColor)
+    if h is not None:
+        hsv.hsv_h = h
+    if s is not None:
+        hsv.hsv_s = s
+    if v is not None:
+        hsv.hsv_v = v
+    return convert_color(hsv, sRGBColor).get_value_tuple()
