5b0e8a6110696a2e61aaebcc882323868487ec01
Christian Brodbeck <christianmbrodbeck@gmail.com>
2020-07-01 14:40:19 -0400

    API:  expose pipeline.EpochCollection

--- eelbrain/pipeline.py
+++ eelbrain/pipeline.py
@@ -2,7 +2,7 @@ from ._utils import _deprecated_alias
 
 from ._experiment.mne_experiment import MneExperiment
 from ._experiment.preprocessing import RawSource, RawFilter, RawICA, RawMaxwell, RawReReference, RawApplyICA
-from ._experiment.epochs import ContinuousEpoch, PrimaryEpoch, SecondaryEpoch, SuperEpoch
+from ._experiment.epochs import ContinuousEpoch, EpochCollection, PrimaryEpoch, SecondaryEpoch, SuperEpoch
 from ._experiment.groups import Group, SubGroup
 from ._experiment.parc import SubParc, CombinationParc, FreeSurferParc, FSAverageParc, SeededParc, IndividualSeededParc
 from ._experiment.test_def import ANOVA, TTestOneSample, TTestIndependent, TTestRelated, TContrastRelated, TwoStageTest, ROITestResult, ROI2StageResult
