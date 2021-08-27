af2be11753ed90ca465126574dd84743c4a06773
Christian Brodbeck <christianmbrodbeck@gmail.com>
2021-08-27 10:11:07 -0400

    FIX PlotData.from_args():  accept iterators

--- eelbrain/_data_obj.py
+++ eelbrain/_data_obj.py
@@ -10945,3 +10945,6 @@ CellArg = Union[str, Tuple[str, ...]]
 IndexArg = Union[Var, np.ndarray, str]
 ModelArg = Union[Model, Var, CategorialArg]
 UVArg = Union[VarArg, CategorialArg]
+
+# Types that can be coerced to NDVar with asndvar(); lists of those can be too
+NDVarTypes = (NDVar, str, MNE_RAW, MNE_EPOCHS, MNE_EVOKED)
