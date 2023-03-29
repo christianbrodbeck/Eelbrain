57e2878d669ed40813e68294980ce1ba9b26c810
Christian Brodbeck <christianmbrodbeck@gmail.com>
2023-03-29 14:58:45 -0400

    ENH gammtone_bank:  integrate over cycles
    Previously integration window was constant

--- eelbrain/_ndvar/gammatone.py
+++ eelbrain/_ndvar/gammatone.py
@@ -1,16 +1,16 @@
 # Author: Christian Brodbeck <christianbrodbeck@nyu.edu>
+from itertools import repeat
 from math import floor
+from typing import Literal
 
 from eelbrain import NDVar, Scalar, UTS
 from eelbrain._utils import tqdm
 from numba import njit, prange
 import numpy
 
-from .uts import pad as _pad_func
-
 
 @njit(parallel=True)
-def aggregate(xf: numpy.ndarray, n_samples: int, step: float, n_window: int):
+def aggregate_left(xf: numpy.ndarray, n_samples: int, step: float, n_window: int):
     xf **= 2
     out = numpy.empty(n_samples)
     for i in prange(n_samples):
@@ -19,15 +19,28 @@ def aggregate(xf: numpy.ndarray, n_samples: int, step: float, n_window: int):
     return out
 
 
+@njit(parallel=True)
+def aggregate_right(xf: numpy.ndarray, n_samples: int, step: float, n_window: int):
+    xf **= 2
+    out = numpy.empty(n_samples)
+    for i in prange(n_samples):
+        stop = int(round(i * step)) + 1
+        start = stop - n_window
+        if start < 0:
+            start = 0
+        out[i] = xf[start: stop].mean()
+    return out
+
+
 def gammatone_bank(
         wav: NDVar,
         f_min: float,
         f_max: float,
         n: int,
-        integration_window: float = 0.010,  # TODO: is this a good default?
         tstep: float = None,
-        location: str = 'right',
-        pad: bool = True,
+        integration_cycles: float = None,
+        integration_window: float = None,
+        location: Literal['left', 'right', 'center'] = 'right',
         name: str = None,
 ) -> NDVar:
     """Gammatone filterbank response
@@ -42,11 +55,14 @@ def gammatone_bank(
         Upper frequency cutoff.
     n
         Number of filter channels.
-    integration_window : scalar
-        Integration time window in seconds (default 10 ms).
-    tstep : scalar
+    tstep
         Time step size in the output (default is same as ``wav``).
-    location : str
+    integration_cycles
+        Number of cycles over which to integrate filter output (default 2).
+    integration_window
+        Integration time window in seconds (can be specified as alternative to
+        ``integration_cycles``; e.g. ``0.010`` for 10 ms).
+    location
         Location of the output relative to the input time axis:
 
         - ``right``: gammatone sample at end of integration window (default)
@@ -76,39 +92,37 @@ def gammatone_bank(
     except ModuleNotFoundError:
         raise ModuleNotFoundError("gammatone module not installed. Install using:\n\n  $ pip install https://github.com/christianbrodbeck/gammatone/archive/fmax.zip")
 
+    assert location in ('left', 'right')
+    fs = 1 / wav.time.tstep
+    cfs = centre_freqs(fs, n, f_min, f_max)
+    if integration_window is None:
+        if integration_cycles is None:
+            integration_cycles = 2
+        integration_window_sec = integration_cycles / cfs
+        integration_window_lens = numpy.ceil(integration_window_sec / wav.time.tstep).astype(int)
+    elif integration_cycles is not None:
+        raise ValueError(f"{integration_cycles=}, {integration_window=} (parameters are mutually exclusive")
+    else:
+        integration_window_len = int(round(integration_window * fs))
+        integration_window_lens = repeat(integration_window_len)
+
     wav_ = wav
     tmin = wav.time.tmin
-    if location == 'left':
-        if pad:
-            wav_ = _pad_func(wav, wav.time.tmin - integration_window)
-    elif location == 'right':
-        if pad:
-            wav_ = _pad_func(wav, tstop=wav.time.tstop + integration_window)
-        else:
-            tmin += integration_window
-    elif location == 'center':
-        dt = integration_window / 2
-        if pad:
-            wav_ = _pad_func(wav, wav.time.tmin - dt, wav.time.tstop + dt)
-        else:
-            tmin += dt
-    else:
-        raise ValueError(f"{location=}")
-    fs = 1 / wav.time.tstep
     if tstep is None:
         tstep = wav.time.tstep
     wave = wav_.get_data('time')
-    # based on gammatone library, rewritten to reduce memory footprint
-    cfs = centre_freqs(fs, n, f_min, f_max)
-    integration_window_len = int(round(integration_window * fs))
-    output_n_samples = floor((len(wave) - integration_window_len) * wav.time.tstep / tstep)
+    # based on gammatone library gtgram, rewritten to reduce memory footprint
+    output_n_samples = floor(len(wave) * wav.time.tstep / tstep)
     output_step = tstep / wav.time.tstep
     output_data = numpy.empty((n, output_n_samples))
     disable = n * output_n_samples < 200_000  # 100 bands * 2 s * 1000 Hz
-    for i, cf in tqdm(enumerate(reversed(cfs)), "Gammatone filterbank", total=len(cfs), unit='band', disable=disable):
+    for i, cf, window in tqdm(zip(range(n-1, -1, -1), cfs, integration_window_lens), "Gammatone filterbank", total=len(cfs), unit='band', disable=disable):
         fcoefs = numpy.flipud(make_erb_filters(fs, cf))
         xf = erb_filterbank(wave, fcoefs)
-        output_data[i] = aggregate(xf[0], output_n_samples, output_step, integration_window_len)
+        if location == 'left':
+            output_data[i] = aggregate_left(xf[0], output_n_samples, output_step, window)
+        else:
+            output_data[i] = aggregate_right(xf[0], output_n_samples, output_step, window)
     output_data = numpy.sqrt(output_data, out=output_data)
     # package output
     freq_dim = Scalar('frequency', cfs[::-1], 'Hz')
