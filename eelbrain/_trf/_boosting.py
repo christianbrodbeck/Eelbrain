fc3e14f98a193e2a28b0a99e0f852bcf02c98f2b
Christian Brodbeck <christianmbrodbeck@gmail.com>
2024-01-16 12:22:36 -0500

    FIX:  default BoostingResult.eelbrain_version

--- eelbrain/_trf/_boosting.py
+++ eelbrain/_trf/_boosting.py
@@ -219,7 +219,7 @@ class BoostingResult(PickleableDataClass):
     # store the version of the boosting algorithm with which model was fit
     version: int = 14  # file format (updates when re-saving)
     algorithm_version: int = -1  # do not change when re-saving
-    eelbrain_version: int = '< 0.39.6'  # do not change when re-saving
+    eelbrain_version: int = '< 0.40'  # do not change when re-saving
     # debug parameters
     y_pred: NDVar = None
     fit: Boosting = None
