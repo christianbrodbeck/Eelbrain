d60cbe59482c228fdab60f20a298bd1b8ea42260
Christian Brodbeck <christianmbrodbeck@gmail.com>
2024-02-20 08:23:53 -0500

    TEST:  remove SampleExperiment.meg_system

--- examples/experiment/sample_experiment.py
+++ examples/experiment/sample_experiment.py
@@ -22,7 +22,6 @@ class SampleExperiment(MneExperiment):
 
     owner = "me@nyu.edu"
 
-    meg_system = 'neuromag306'
     stim_channel = 'STI 014'
     merge_triggers = -1  # ignore events of duration 1
 
