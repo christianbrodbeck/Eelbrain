888bc780acd7478e11a1fdc4898ca694e834d4ce
Christian Brodbeck <christianmbrodbeck@gmail.com>
2024-08-06 07:43:05 -0400

    TEST gammatone_bank()

---
+++ eelbrain/_ndvar/tests/test_gammatone.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import pytest
+
+from eelbrain import load, gammatone_bank
+from eelbrain.testing import assert_dataobj_equal
+from eelbrain.testing.data import TEST_DATA_DIRECTORY, generate_sound
+
+
+def test_gammatone_bank():
+    target_path = TEST_DATA_DIRECTORY / 'gammatone.pickle'
+    if not target_path.exists():
+        pytest.skip(f"Test data missing: {target_path}")
+    target = load.unpickle(target_path)
+
+    sound = generate_sound()
+    gt = gammatone_bank(sound, 20, 2000, 32, 1 / 100)
+    assert_dataobj_equal(gt, target)
+
+    target_left = load.unpickle(TEST_DATA_DIRECTORY / 'gammatone-left.pickle')
+    gt = gammatone_bank(sound, 20, 2000, 32, 1 / 100, location='left')
+    assert_dataobj_equal(gt, target_left)
