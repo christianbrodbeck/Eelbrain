8d183b9a7cab2403d39b738db69866ef6179773b
Christian Brodbeck <christianmbrodbeck@gmail.com>
2025-03-01 13:21:21 -0500

    AMD

--- eelbrain/datasets/_alice.py
+++ eelbrain/datasets/_alice.py
@@ -3,6 +3,7 @@ import hashlib
 import shutil
 from pathlib import Path
 from urllib.request import urlretrieve
+import zipfile
 
 from pooch import file_hash
 
@@ -26,14 +27,14 @@ def get_alice_path(
     ]
 
     for url, hash in urls:
-        temp_file_name, header = urlretrieve(url)
-        with open(temp_file_name, "rb") as f:
+        temp_file_path, header = urlretrieve(url)
+        with open(temp_file_path, "rb") as f:
             for chunk in iter(lambda: f.read(1048576), b""):
                 md5.update(chunk)
         file_hash = md5.hexdigest()
         if file_hash != hash:
             raise RuntimeError(f'Hash mismatch for {url}')
-        with zipfile.ZipFile(temp_file_name, 'r') as f:
+        with zipfile.ZipFile(temp_file_path, 'r') as f:
             f.extractall(path)
-        Path(temp_file_name).unlink()
+        Path(temp_file_path).unlink()
     return path
