7aaac8a8d239901932a9053e588acc2a86f04931
Samira Saljoughi <104131510+Samira-s@users.noreply.github.com>
2026-07-23 12:25:24 -0400

    Fix: set name on Datalist returned by _load_predictor_nested (#214)

--- eelbrain/_experiment/trf/nodes.py
+++ eelbrain/_experiment/trf/nodes.py
@@ -394,7 +394,7 @@ class TRFDerivative(Derivative[object]):
             x = filter_predictor(x, self.raw, ctx.state['raw'], filter_x)
             x.name = term.string
             xs.append(x)
-        return Datalist(xs)
+        return Datalist(xs, name=term.string)
 
     def _load_subject_predictor(self, ctx: Request, predictor: SubjectUTSPredictor, term: Term, ds, y, filter_x: bool | str, is_nested: bool) -> NDVar | Datalist:
         "Cut recording-long predictors into response cases"
